2799b757f6a414136083794462074d332ab9b29e
hamishneu <hamishneu@gmail.com>
2024-12-07 17:28:47 +1100

    update

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,11 +1,10 @@
 return {
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters = {
-        markdownlint = {
-          args = { "--disable", "MD013", "--" },
-        },
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", "/home/pcino/.markdownlint-cli2.yaml", "--" },
       },
     },
   },
